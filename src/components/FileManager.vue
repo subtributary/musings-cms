ef5798ebf92ab17c08d99860148c8d0e00a68822
Nathan <nathan.belue@gmail.com>
2026-07-21 17:31:26 -0500

    removed svar file manager

---
+++ src/components/FileManager.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { Filemanager, Willow } from '@svar-ui/vue-filemanager'
+import { RestDataProvider } from '@svar-ui/filemanager-data-provider'
+
+const restProvider = new RestDataProvider(import.meta.env.VITE_API_URL)
+const data = await restProvider.loadFiles('/')
+</script>
+
+<template>
+  <Willow>
+    <Filemanager :data="data" />
+  </Willow>
+</template>
--- src/views/FilesView.vue
+++ src/views/FilesView.vue
@@ -1,48 +1,20 @@
 <script setup lang="ts">
-import { Filemanager, Willow } from '@svar-ui/vue-filemanager'
+import { Willow } from '@svar-ui/vue-filemanager'
 import '@svar-ui/vue-filemanager/all.css'
-
-const rawdata = [
-  {
-    id: '/Code',
-    size: 4096,
-    date: new Date(2023, 11, 2, 17, 25),
-    type: 'folder',
-  },
-  {
-    id: '/Music',
-    size: 4096,
-    date: new Date(2023, 11, 1, 14, 45),
-    type: 'folder',
-  },
-
-  {
-    id: '/Info.txt',
-    size: 1000,
-    date: new Date(2023, 10, 30, 6, 13),
-    type: 'file',
-  },
-  {
-    //parent: "/Code/Datepicker",
-    id: '/Code/Datepicker/Year.vue',
-    size: 1595,
-    date: new Date(2023, 11, 7, 15, 23),
-    type: 'file',
-  },
-  {
-    id: '/Pictures/162822515312968813.png',
-    size: 510885,
-    date: new Date(2023, 11, 1, 14, 45),
-    type: 'file',
-  },
-]
+import FileManager from '@/components/FileManager.vue'
+import BusyIndicator from '@/components/BusyIndicator.vue'
 </script>
 
 <template>
   <main>
-    <Willow>
-      <Filemanager :data="rawdata" />
-    </Willow>
+    <Suspense>
+      <Willow>
+        <FileManager />
+      </Willow>
+      <template #fallback>
+        <BusyIndicator />
+      </template>
+    </Suspense>
   </main>
 </template>
 
